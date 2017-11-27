bde6a78d38c693978b6e0128d8897fe5ffcacd8f
Shyam Saladi <smsaladi@gmail.com>
2017-11-27 13:19:35 -0800

    moving CNN stuff to its own branch

--- CNNtesting/WAVGeneration.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import scipy.misc
-#Hopefully we can find some .wav files to plot
-images = [] #Use images.append(img) to add to this. Convert to np array at the end
-arrays = [] #Use arrays.append(arr) to add to this. Convert to np array at the end
