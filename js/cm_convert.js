e039509196ffd9215a0e38a102226d035edc84db
Shyam Saladi <smsaladi@gmail.com>
2018-05-06 17:42:17 -0700

    move to OOP with converter class -- will make switch to kdtree easier

--- js/cm_convert.js
+++ js/cm_convert.js
@@ -2,8 +2,11 @@
  * Image conversion
  * All the heavy lifting done here
  *
- * Uses chroma.js for color conversions. Make sure that's loaded!
- * https://github.com/gka/chroma.js
+ * Libraries:
+ *    chroma.js       https://github.com/gka/chroma.js
+ * 
+ * Make sure they are loaded!
+ * 
  */
 
 function getSelection(element, selName) {
@@ -18,44 +21,23 @@ function getSelection(element, selName) {
  */
 function convertFigure(figSection) {
   var fig = figSection.getElementsByClassName('figure-container')[0];
-  var unmappedColors = false;
-
   var fromMapName = getSelection(figSection, 'from-colormap');
   var toMapName = getSelection(figSection, 'to-colormap');
-  var toCm = chroma.scale(colorscales[toMapName]);
-
-  if (fromMapName == 'jet') {
-    var fromCm = createJetInLabSpace();
-  } else {
-    var fromCm = convertHexToLab(colorscales[fromMapName]);
-  }
-
-  // check that the pixel doesn't correspond more closely to black or white
-  // presumably text on the page
-  var outOfScope = [chroma([0, 0, 0]).lab(),         // black
-                    chroma([255, 255, 255]).lab()];   // white
-
+  var lookup = new ColormapLookup(fromMapName, toMapName);
   if (figSection.classList.contains("canvas")) {
-    unmappedColors = convertCanvas(fig.getElementsByTagName("canvas")[0],
-                                   fromCm, outOfScope, unmappedColors, toCm);
+    convertCanvas(fig.getElementsByTagName("canvas")[0], lookup.toNewRGB);
   }
   else if (figSection.classList.contains("svg")) {
-    var toNewCm = function (frac, cmap=toCm) {
-      return cmap(frac).hex();
-    };
-
     var elems = fig.firstElementChild.getElementsByTagName('*');
     for (var i = 0; i < elems.length; i++) {
       // various locations for color-type attributes
       if (elems[i].getAttribute('fill') != null)
-        [elems[i].attributes.fill.value, unmappedColors] =
-          invertColor(elems[i].getAttribute('fill'), fromCm, outOfScope, toNewCm, unmappedColors);
+        elems[i].attributes.fill.value =
+          lookup.toNewHex(elems[i].getAttribute('fill'));
       else if (elems[i].style.fill != "" && elems[i].style.fill != "none")
-        [elems[i].style.fill, unmappedColors] =
-          invertColor(elems[i].style.fill, fromCm, outOfScope, toNewCm, unmappedColors);
+        elems[i].style.fill = lookup.toNewHex(elems[i].style.fill);
       else if (elems[i].style.fill != "" && elems[i].style.fill != "none")
-        [elems[i].style.stroke, unmappedColors] =
-          invertColor(elems[i].style.stroke, fromCm, outOfScope, toNewCm, unmappedColors);
+        elems[i].style.stroke = lookup.toNewHex(elems[i].style.stroke);
       // TODO: other elements?
       // gradients?
     }
@@ -74,7 +56,7 @@ function convertFigure(figSection) {
           canvas.height = evt.target.height;
           ctx.drawImage(evt.target, 0, 0);
 
-          var unmappedColors = convertCanvas(canvas, fromCm, outOfScope, unmappedColors, toCm);
+          convertCanvas(canvas, lookup.toNewRGB);
 
           img.setAttribute("xlink:href", canvas.toDataURL());
       };
@@ -82,7 +64,7 @@ function convertFigure(figSection) {
     }
   }
 
-  if (unmappedColors) {
+  if (lookup.unmapped) {
     var alert = document.createElement("div");
     alert.className = "alert alert-warning";
     alert.innerText = "Your image contains some colors (e.g. white, black) not in the jet colormap. We have left these alone and converted the rest of the image.";
@@ -93,119 +75,129 @@ function convertFigure(figSection) {
 /**
  * Conversion for canvas objects
  * @param {number[]} cvs: the canvas
- * @param {number[][]} jetInLabSpace: original colormap to match to (in lab space)
- * @param {number[][]} outOfScope: colors to ignore
- * @returns whether there were unmapped colors
+ * @param {function} func: callback to convert each pixel/RGB array 
  */
-function convertCanvas(cvs, jetInLabSpace, outOfScope, unmappedColors, toCm) {
+function convertCanvas(cvs, func) {
   var ctx = cvs.getContext('2d');
   var imageData = ctx.getImageData(0, 0, cvs.width, cvs.height);
-  var toNewCm = function (frac, cmap=toCm) {
-    return cmap(frac).rgb();
-  };
 
   // find closest viridis color for each jet pixel
   // i += 4 since each pixel is [R, G, B, alpha]
   for (var i = 0; i < imageData.data.length; i += 4)
-      [[imageData.data[i],
+      [imageData.data[i],
        imageData.data[i + 1],
-       imageData.data[i + 2]], unmappedColors] =
-        invertColor(Array.from(imageData.data.slice(i, i + 3)),
-                    jetInLabSpace, outOfScope, toNewCm, unmappedColors);
+       imageData.data[i + 2]] =
+        func(Array.from(imageData.data.slice(i, i + 3)));
   ctx.putImageData(imageData, 0, 0);
 
-  return unmappedColors;
 }
 
 /**
  * Finds the value that gives rise to the given color within the original space
  * @param {number[]} value: color in RGB space
- * @param {number[][]} origValues: original colormap to match to (in lab space)
- * @param {number[][]} outOfScope: colors to ignore
- * @param {function} cm: callable that accepts a fractional value
+ * @param {number[][]} cm_kdt: original colormap as kd tree
+ * @param {number[][]} maxDist: colors to ignore
  * @returns {number} outcome of callable
  */
-function invertColor(value, origValues, outOfScope, cm, umapIn) {
-  if (value == "none")
-    return [value, true];
-  var mappedValue = invertValue(chroma(value).lab(), origValues, outOfScope);
-  if (mappedValue == null)
-    return [value, true];
-  else
-    return [cm(mappedValue), umapIn];
-}
+class ColormapLookup {
+  constructor(fromMapName, toMapName, maxDist = 5) {
+    var self = this;
+    self.maxDist = maxDist;
+    self.unmapped = false;
+    self.reset = function() {
+      self.unmapped = false;
+    };
 
-/**
- * Finds the value that gives rise to the given color within the original space
- * @param {number[]} value: outcome of original space
- * @param {number[][]} origValues: original space to match to
- * @param {number[][]} outOfScope: values to ignore
- * @returns {number} mapped value (fractional)
- */
-function invertValue(value, origValues, outOfScope) {
-  var lowestIndex = 0;
-  var lowestDistance = euclideanDist(value, origValues[lowestIndex]);
-  for (var i = 1; i < origValues.length; i++)
-    if (euclideanDist(origValues[i], value) < lowestDistance) {
-      lowestDistance = euclideanDist(origValues[i], value);
-      lowestIndex = i;
-    }
+    if (fromMapName == 'jet') {
+      self.fromCm = self.createJetInLabSpace();
+    } else
+      self.fromCm = self.convertHexToLab(colorscales[fromMapName]);
+    // self.kdt = createKDTree(self.fromCm);
+    self.toCm = chroma.scale(colorscales[toMapName]);
 
-  for (var i = 0; i < outOfScope.length; i++)
-    if (euclideanDist(outOfScope[i], value) < lowestDistance)
-      return null;
+    /**
+     * Finds the value that gives rise to the given color within the original space
+     * @param {number[]} value: color in RGB space
+     */
+    self.toFrac = function(value) {
+      if (value == "none"){
+        self.unmapped = true;
+        return -1;
+      }
 
-  return lowestIndex / origValues.length;
-}
+      // var mappedValue = self.kdt.nn(chroma(value).lab(), self.maxDist);
+      var mappedValue = invertValue(chroma(value).lab(), self.fromCm); 
+      return mappedValue / self.fromCm.length;
+    };
 
+    self.toNewRGB = function(value) {
+      var frac = self.toFrac(value);
+      if (frac < 0) {
+        self.unmapped = true;
+        return value;
+      } else
+        return self.toCm(frac).rgb();
+    };
 
-/**
- * Create an array of Jet values in the Lab colorspace
- *
- * @param {number} [bitDepth]
- * @returns {number[number[]]}
- */
-function createJetInLabSpace(bitDepth = 8) {
-  var jetInLab = new Array(Math.pow(2, bitDepth));
-
-  for (var i = 0; i < jetInLab.length; i++) {
-    var rgbJet = [0, 0, 0];
-    var iFrac = i / jetInLab.length;
-
-    // find the fractional R, G, B colors for each Jet value
-    if (iFrac < .125) {
-      rgbJet[2] = 0.5 + 4 * iFrac;
-    } else if (iFrac < 0.375) {
-      rgbJet[1] = -0.5 + 4 * iFrac;
-      rgbJet[2] = 1;
-    } else if (iFrac < 0.625) {
-      rgbJet[0] = -1.5 + 4 * iFrac;
-      rgbJet[1] = 1;
-      rgbJet[2] = 2.5 - 4 * iFrac;
-    } else if (iFrac < 0.875) {
-      rgbJet[0] = 1;
-      rgbJet[1] = 3.5 - 4 * iFrac;
-    } else {
-      rgbJet[0] = 4.5 - 4 * iFrac;
-    }
-
-    // Convert fractional values into ints of the given bitDepth
-    for (var j = 0; j < 3; j++)
-      rgbJet[j] = Math.round(rgbJet[j] * (jetInLab.length - 1));
-
-    jetInLab[i] = chroma(rgbJet).lab();
+    self.toNewHex = function(value) {
+      var frac = self.toFrac(value);
+      if (frac < 0) {
+        self.unmapped = true;
+        return value;
+      } else
+        return self.toCm(frac).hex();
+    };
   }
-  return jetInLab;
-}
 
-function convertHexToLab(arr) {
-  var labArr = new Array(arr.length);
-  for (var i = 0; i < arr.length; i++) {
-    labArr[i] = chroma(arr[i]).lab();
+  convertHexToLab(arr) {
+    var labArr = new Array(arr.length);
+    for (var i = 0; i < arr.length; i++) {
+      labArr[i] = chroma(arr[i]).lab();
+    }
+    return labArr;
   }
-  return labArr;
-}
 
+  /**
+   * Create an array of Jet values in the Lab colorspace
+   * 
+   *  UNUSED 
+   *
+   * @param {number} [bitDepth]
+   * @returns {number[number[]]}
+   */
+  createJetInLabSpace(bitDepth = 8) {
+    var jetInLab = new Array(Math.pow(2, bitDepth));
+
+    for (var i = 0; i < jetInLab.length; i++) {
+      var rgbJet = [0, 0, 0];
+      var iFrac = i / jetInLab.length;
+
+      // find the fractional R, G, B colors for each Jet value
+      if (iFrac < .125) {
+        rgbJet[2] = 0.5 + 4 * iFrac;
+      } else if (iFrac < 0.375) {
+        rgbJet[1] = -0.5 + 4 * iFrac;
+        rgbJet[2] = 1;
+      } else if (iFrac < 0.625) {
+        rgbJet[0] = -1.5 + 4 * iFrac;
+        rgbJet[1] = 1;
+        rgbJet[2] = 2.5 - 4 * iFrac;
+      } else if (iFrac < 0.875) {
+        rgbJet[0] = 1;
+        rgbJet[1] = 3.5 - 4 * iFrac;
+      } else {
+        rgbJet[0] = 4.5 - 4 * iFrac;
+      }
+
+      // Convert fractional values into ints of the given bitDepth
+      for (var j = 0; j < 3; j++)
+        rgbJet[j] = Math.round(rgbJet[j] * (jetInLab.length - 1));
+
+      jetInLab[i] = chroma(rgbJet).lab();
+    }
+    return jetInLab;
+  }
+}
 
 /**
  * Simple Euclidean distance for 3 dimensions
@@ -218,4 +210,32 @@ function euclideanDist(a, b) {
   return Math.sqrt(Math.pow(a[0] - b[0], 2) +
                    Math.pow(a[1] - b[1], 2) +
                    Math.pow(a[2] - b[2], 2));
+}
+
+/**
+ * Finds the value that gives rise to the given color within the original space
+ * @param {number[]} value: outcome of original space
+ * @param {number[][]} origValues: original space to match to
+ * @param {number[][]} outOfScope: values to ignore
+ * @returns {number} mapped value (fractional)
+ */
+function invertValue(value, origValues) {
+  // check that the pixel doesn't correspond more closely to black or white
+  // presumably text on the page
+  var outOfScope = [chroma([0, 0, 0]).lab(),         // black
+                    chroma([255, 255, 255]).lab()];   // white
+
+  var lowestIndex = 0;
+  var lowestDistance = euclideanDist(value, origValues[lowestIndex]);
+  for (var i = 1; i < origValues.length; i++)
+    if (euclideanDist(origValues[i], value) < lowestDistance) {
+      lowestDistance = euclideanDist(origValues[i], value);
+      lowestIndex = i;
+    }
+
+  for (var i = 0; i < outOfScope.length; i++)
+    if (euclideanDist(outOfScope[i], value) < lowestDistance)
+      return -1;
+
+  return lowestIndex;
 }
